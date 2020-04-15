3875e212fbc3b2a3f46452fe5fdd6d2fe2750d8d
Kevin Tang <kevintang300@gmail.com>
2020-04-14 22:39:44 -0700

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Kevin Tang on 4/14/20.
+//  Copyright © 2020 ktang300. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
