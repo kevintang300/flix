96422cb9f07bc46bafca5f7e862db3b06d59be9d
Kevin Tang <kevintang300@gmail.com>
2020-04-19 22:43:09 -0700

    assignment 2 update

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Kevin Tang on 4/19/20.
+//  Copyright © 2020 ktang300. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+
+    
+}
